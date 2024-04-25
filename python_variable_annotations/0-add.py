16ecf4ab71cf0be2b63272e27a9064b7bc5f9376
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-04-25 19:43:55 -0400

    Coding

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+""" Annotation add  """
 
 def add(a: float, b: float) -> float:
+    """ annotated function """
     return a + b
 
+""" Calling annotated function """
 resultado = add(2.5, 3.5)
 print(resultado)
