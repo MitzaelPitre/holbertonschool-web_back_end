3dfdd1aa782becbbc0620511f0bbe756636ab753
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-05-02 22:53:21 -0400

    Coding

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -6,10 +6,10 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     data = MongoClient().logs.nginx
     print(f'{data.count_documents({})} logs')
-    print('   Methods:')
-    print(f'\tmethod GET: {data.count_documents({"method": "GET"})}')
-    print(f'\tmethod POST: {data.count_documents({"method": "POST"})}')
-    print(f'\tmethod PUT: {data.count_documents({"method": "PUT"})}')
-    print(f'\tmethod PATCH: {data.count_documents({"method": "PATCH"})}')
-    print(f'\tmethod DELETE: {data.count_documents({"method": "DELETE"})}')
+    print('    Methods:')
+    print(f'        method GET: {data.count_documents({"method": "GET"})}')
+    print(f'        method POST: {data.count_documents({"method": "POST"})}')
+    print(f'        method PUT: {data.count_documents({"method": "PUT"})}')
+    print(f'        method PATCH: {data.count_documents({"method": "PATCH"})}')
+    print(f'        method DELETE: {data.count_documents({"method": "DELETE"})}')
     print(f'{data.count_documents({"method": "GET", "path": "/status"})} status check')
