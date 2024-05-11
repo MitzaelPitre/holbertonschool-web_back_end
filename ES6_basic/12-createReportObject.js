b3186450457d86d1d8ef6425140d70878448b167
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-05-10 20:27:18 -0400

    Coding

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,6 +1,6 @@
 export default function createReportObject(employeesList) {
   return {
-    allEmployees: { ...employeesList }, // Coma añadida aquí
+    allEmployees: { ...employeesList }, // Aquí debería estar la coma
     getNumberOfDepartments() {
       return Object.keys(this.allEmployees).length;
     }
